fc0f7ba3e458049f5f3f524c12c9e9e7470706d2
Clesean Glover <cg8480a@american.edu>
2026-03-29 22:36:29 -0400

    The Shiny R assignment includes an option for the user to choose between a boxplot and a histogram.

--- app.R
+++ app.R
@@ -5,19 +5,40 @@ n <- 200
 # Define the UI
 ui <- bootstrapPage(
   titlePanel("Clesean Glover"),
-  numericInput('n', 'Number of obs', n),
-  radioButtons("colors", "Choose a color?",
-      choices = c("green", "red", "blue", "yellow")),
-  plotOutput('plot')
-)
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("PlotType", "Select a plottype:",
+                  choices = c("Boxplot", "Histogram")),
+      numericInput('n', 'Number of obs', n),
+      radioButtons("colors", "Choose a color?",
+                   choices = c("green", "red", "blue", "yellow")),
+      plotOutput('plot')
+  ),
+  
+  mainPanel(
+      plotOutput('distPlot')
+      )
+    )
+  )
+
+  
+
 
 
 # Define the server code
 server <- function(input, output) {
-  output$plot <- renderPlot({
-    boxplot(runif(input$n), col = input$colors)
-            })
-}
+  output$distPlot <- renderPlot({
+    
+    if (input$PlotType == "Histogram") {
+      hist(runif(input$n), col = input$colors)
+    } else {
+      boxplot(runif(input$n), col = input$colors)
+    }
+})
+    
+}    
+
 
 # Return a Shiny app object
+  
 shinyApp(ui = ui, server = server)
